53cbf0c30c8129ca8cf0b79c3feb29d5ed599dcf
Damián Argote <DArgote@coasa.com.ar>
2022-06-29 00:20:15 -0300

    variables de entorno

--- src/app/modules/productos/productos.service.ts
+++ src/app/modules/productos/productos.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Producto } from './models/producto.model';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductosService {
   public _productos: Producto[] = [];
-  private baseUrl = 'http://localhost:4000/api/v1';
+  private baseUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) {}
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://damian-productos-api.herokuapp.com/api/v1',
 };
